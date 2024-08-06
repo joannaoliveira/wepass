86e9e67b19c3fb72ffb60cb562ffa962b1cb6133
Joanna Oliveira <joannaoliveira44@gmail.com>
2024-08-06 14:59:13 -0300

    inicio

---
+++ assets/js/global.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+
+    var behavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+    },
+    options = {
+        onKeyPress: function (val, e, field, options) {
+            field.mask(behavior.apply({}, arguments), options);
+        }
+    };    
+    $('.phone').mask(behavior, options);
+
+    $(".solicitar-analise").on( "click", function() {
+        $("#frmCotacao").get(0).scrollIntoView({behavior: 'smooth'});
+        return false;
+    });
+
+    $("#frmCotacao").validate({
+        rules: {
+            iNome: "required",
+            iSobrenome: "required",
+            iEmail: {
+                required: true,
+                email: true
+            },
+            iWhatsapp: "required",            
+            iTpDivida: "required",            
+            iValor: "required"
+        },
+        messages: {
+            iNome: "Campo obrigatório",
+            iSobrenome: "Campo obrigatório",
+            iEmail: {
+                required: "Campo obrigatório",
+                email: "Digite um e-mail válido"
+            },
+            iWhatsapp: "Campo obrigatório",
+            iTpDivida: "Campo obrigatório",
+            iValor: "Campo obrigatório"
+        }
+    });
+
+});
